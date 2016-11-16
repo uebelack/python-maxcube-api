9039ad00fd075a305ef925b0ae2edc8c1a7a41d0
joconcepts <jonas@jonas-keidel.de>
2016-11-16 18:21:44 +0100

    add type_name and todict method
    Signed-off-by: joconcepts <jonas@jonas-keidel.de>

--- maxcube/device.py
+++ maxcube/device.py
@@ -5,15 +5,41 @@ MAX_WALL_THERMOSTAT = 3
 MAX_WINDOW_SHUTTER = 4
 MAX_PUSH_BUTTON = 5
 
+MAX_DEVICES = [
+        "cube",
+        "thermostat",
+        "thermostat_plus",
+        "wall_thermostat",
+        "window_shutter",
+        "push_button"
+]
+
 MAX_DEVICE_MODE_AUTOMATIC = 0
 MAX_DEVICE_MODE_MANUAL = 1
 MAX_DEVICE_MODE_VACATION = 2
 MAX_DEVICE_MODE_BOOST = 3
 
+MAX_DEVICE_MODES = [
+        "auto",
+        "manual",
+        "vacation",
+        "boost"
+]
 
 class MaxDevice(object):
     def __init__(self):
+        self.cube = None
         self.type = None
+        self.type_name = None
         self.rf_address = None
         self.room_id = None
         self.name = None
+
+    def todict(self):
+        d = {
+                "type": self.type_name,
+                "rf_address": self.rf_address,
+                "room_id": self.room_id,
+                "name": self.name
+        }
+        return d
