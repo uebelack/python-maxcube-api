92b89da47f36fe70287fc4d82152ba774298921f
Wolf-Bastian Pöttner <bastian@poettner.de>
2017-02-05 14:49:08 +0100

    Increase version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('LICENSE') as f:
 
 setup(
     name='maxcube-api',
-    version='0.0.2',
+    version='0.0.3',
     description='eQ-3/ELV MAX! Cube Python API',
     long_description=readme,
     author='David Uebelacker',
