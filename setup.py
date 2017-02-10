9458b66cd8291355d5c8feee6b00c12d010ad6d9
David Uebelacker <david@uebelacker.ch>
2017-02-10 08:31:00 +0100

    increased version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('LICENSE') as f:
 
 setup(
     name='maxcube-api',
-    version='0.0.3',
+    version='0.1.0',
     description='eQ-3/ELV MAX! Cube Python API',
     long_description=readme,
     author='David Uebelacker',
