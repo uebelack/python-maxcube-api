045f850caf7ca3a775c1ae823e0b747fefb62faa
joconcepts <jonas@jonas-keidel.de>
2017-06-09 16:49:57 +0200

    set mode via parameter
    Signed-off-by: joconcepts <jonas@jonas-keidel.de>

--- maxcube/thermostat.py
+++ maxcube/thermostat.py
@@ -17,6 +17,7 @@ class MaxThermostat(MaxDevice):
         d = super(MaxThermostat, self).todict()
         h = {
                 "mode": self.mode_name,
+                "mode_id": self.mode,
                 "min_temperature": self.min_temperature,
                 "max_temperature": self.max_temperature,
                 "actual_temperature": self.actual_temperature,
@@ -26,15 +27,16 @@ class MaxThermostat(MaxDevice):
         return { **d, **h }
 
 
-    def set_temperature(self, temperature):
+    def set_temperature(self, temperature, mode="manual"):
         self.cube.logger.debug('Setting temperature for %s to %s!' %(self.rf_address, temperature))
+        mode_id = MAX_DEVICE_MODES.index(mode)
         rf_address = self.rf_address
         if len(rf_address) < 6:
             rf_address = '0' + rf_address
         room = str(self.room_id)
         if self.room_id < 10:
             room = '0' + room
-        target_temperature = int(temperature * 2) + (self.mode << 6)
+        target_temperature = int(temperature * 2) + (mode_id << 6)
 
         byte_cmd = '000440000000' + rf_address + room + hex(target_temperature)[2:]
         self.cube.logger.debug('Request: ' + byte_cmd)
