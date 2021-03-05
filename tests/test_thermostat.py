89efe6e926625d1664c31f7dc20b89f509fea8a9
Unai Uribarri <unaiur@gmail.com>
2021-03-05 21:08:33 +0100

    Update temperature when changing to auto mode
    Use the weekly programme to retrieve current temperature
    when changing to automatic mode.

---
+++ tests/test_thermostat.py
@@ -0,0 +1,10 @@
+
+from unittest import TestCase
+from maxcube.thermostat import MaxThermostat
+
+class TestMessage(TestCase):
+    """ Test Max! thermostat """
+
+    def testGetCurrentTemperatureReturnsNoneIfUninitialized(self):
+        t = MaxThermostat()
+        self.assertIsNone(t.get_current_temp_in_auto_mode())
