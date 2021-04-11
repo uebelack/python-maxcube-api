507f70f2bab6eb7f3cd8bbac9619875aa558bb25
Unai Uribarri <unaiur@gmail.com>
2021-04-11 21:58:46 +0200

    Release version 0.4.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.rst") as f:
 
 setup(
     name="maxcube-api",
-    version="0.4.1",
+    version="0.4.2",
     description="eQ-3/ELV MAX! Cube Python API",
     long_description=readme,
     author="David Uebelacker",
