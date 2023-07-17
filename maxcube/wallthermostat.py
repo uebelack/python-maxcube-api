fdaa93ec0f81435cb70d951eef2dcc4c02020c00
MassiPi <2384381+MassiPi@users.noreply.github.com>
2023-07-17 21:58:59 +0200

    Update wallthermostat.py - Manage programme
    need to manage weekly program also for wall thermostats to fix error in home assistant integration

--- maxcube/wallthermostat.py
+++ maxcube/wallthermostat.py
@@ -1,5 +1,17 @@
+from datetime import datetime
+from typing import Dict, List
+
 from maxcube.device import MODE_NAMES, MaxDevice
 
+PROG_DAYS = [
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+]
 
 class MaxWallThermostat(MaxDevice):
     def __init__(self):
@@ -11,7 +23,8 @@ class MaxWallThermostat(MaxDevice):
         self.actual_temperature = None
         self.target_temperature = None
         self.mode = None
-
+        self.programme: Dict[str, List[Dict[str, int]]] = {}
+        
     def __str__(self):
         return self.describe(
             "WALLTHERMO",
@@ -22,3 +35,16 @@ class MaxWallThermostat(MaxDevice):
             f"comfort={self.comfort_temperature}",
             f"range=[{self.min_temperature},{self.max_temperature}]",
         )
+
+    def get_programmed_temp_at(self, dt: datetime):
+        """Retrieve the programmed temperature at the given instant."""
+        weekday = PROG_DAYS[dt.weekday()]
+        time = f"{dt.hour:02}:{dt.minute:02}"
+        for point in self.programme.get(weekday, []):
+            if time < point["until"]:
+                return point["temp"]
+        return None
+
+    def get_current_temp_in_auto_mode(self):
+        """DEPRECATED: use get_programmed_temp_at instead."""
+        return self.get_programmed_temp_at(datetime.now())
